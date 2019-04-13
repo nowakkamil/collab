f9804727f97f3b81b7ae09d3ecafe631981857ee
nowakkamil <nowakkamil@yahoo.com>
2019-04-13 20:45:29 +0200

    Add ArticleService placeholder

---
+++ Collab.Application/Services/Implementations/ArticleService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using Collab.Data;
+using Collab.Application.Dto;
+
+namespace Collab.Application.Services.Implementations
+{
+    public class ArticleService : IArticleService
+    {
+    }
+}
---
+++ Collab.Application/Services/Interfaces/IArticleService.cs
@@ -0,0 +1,12 @@
+﻿using Collab.Application.Dto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collab.Application.Services
+{
+    public interface IArticleService
+    {
+    }
+}
