5a3a86f868c39076a317af17924f03708b01b7de
Szymon Rozek <rozek1szymon@gmail.com>
2019-04-23 11:42:46 +0200

    re added everything with controller

---
+++ Collab.Web/Controllers/CommentController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Collab.Application.Dtos;
+using Collab.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Collab.Web.Controllers
+{
+    public class CommentController : Controller
+    {
+
+        private readonly ICommentService _commentService;
+
+        public CommentController(ICommentService commentService)
+        {
+            _commentService = commentService ??
+                throw new ArgumentNullException(nameof(commentService));
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCommentById(CommentDto commentDto)
+        {
+            var comment = await _commentService.CreateCommentAsync(commentDto);
+
+            if (comment == null)
+                return NotFound();
+
+            return Ok(comment);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCommentById(int id)
+        {
+            var comment = await _commentService.DeleteCommentByIdAsync(id);
+
+            if (comment == null)
+                return NotFound();
+
+            return Ok(comment);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditComment(CommentDto commentDto)
+        {
+            var comment = await _commentService.EditCommentAsync(commentDto);
+
+            if (comment == null)
+                return NotFound();
+
+            return Ok(comment);
+        }
+    }
+}
